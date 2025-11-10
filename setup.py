6cf6efdba6d40b990487e0eb5337fa25221115c0
xiaoyy20d <xiaoyy20d@gmail.com>
2025-11-10 13:31:08 +0800

    版本

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ if os.path.exists("requirements.txt"):
 
 setup(
     name="cliscript",
-    version="0.1.4",
+    version="0.1.5",
     author="xYuan20d",
     author_email="xiaoyy20d@gmail.com",
     description="A Domain Specific Language for building CLI applications",
