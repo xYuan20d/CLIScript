cb3cc5ed75183d9b3ed1b0fa492a4515c1551030
xiaoyy20d <xiaoyy20d@gmail.com>
2025-11-10 11:34:00 +0800

    更新版本

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("requirements.txt", "r", encoding="utf-8") as f:
 
 setup(
     name="cliscript",
-    version="0.1.0",
+    version="0.1.1",
     author="xYuan20d",
     author_email="xiaoyy20d@gmail.com",
     description="A Domain Specific Language for building CLI applications",
